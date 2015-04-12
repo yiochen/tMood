c5f447f6dc70f6ba3d046d70a8030e6da42778d0
Yiou Chen <chen.yiou1992@gmail.com>
2015-04-12 08:18:15 -0400

    change UI detail

--- src/app.js
+++ src/app.js
@@ -9,16 +9,38 @@ var locationOptions={
   maximumAge:10000,
   timeout:10000
 };
-var splash=new UI.Card({
-  title:"PebbleTweet",
-  subtitle:"Loading.."
+var splash=new UI.Window({
+  fullscree:true,
+  scrollable:true
 });
+//title:"PebbleTweet",
+//subtitle:"Loading.."
+var title=new UI.Text({
+  text:"PeSentiment",
+  size:new Vector2(144,28),
+  font:'gothic-28-bold',
+  position:new Vector2(0,70),
+  textAlign:'center',
+  color:"white"
+});
+splash.add(title);
+splash.title=title;
+
+var subtitle=new UI.Text({
+  text:"loading",
+  size:new Vector2(144,12),
+  position:new Vector2(0,100),
+  textOverflow:"wrap",
+  color:"white"
+});
+splash.add(subtitle);
+splash.subtitle=subtitle;
 splash.show();
 
 //make request about the geolocation
 function locationSuccess(pos) {
   console.log('lat= ' + pos.coords.latitude + ' lon= ' + pos.coords.longitude);
-  splash.title('lat= ' + pos.coords.latitude + ' lon= ' + pos.coords.longitude);
+  splash.subtitle.text('lat= ' + pos.coords.latitude + ' lon= ' + pos.coords.longitude);
   //geotext.text('lat= ' + pos.coords.latitude + ' lon= ' + pos.coords.longitude);
   var URL="https://api.foursquare.com/v2/venues/search?client_id=R355ECNPS1QUQFJIE3NFTF10DTOFPHZTZOXKTCW3PWZ5UD3A &client_secret=4K0IGHZ4MQ55YME315CW2S3VN5WWQMVLNEYK1K1NJKW3R0PS&v=20130815&limit=10&ll="+pos.coords.latitude + ',' + pos.coords.longitude;
   ajax(
@@ -32,7 +54,7 @@ function locationSuccess(pos) {
       console.log(JSON.parse(data).response);
       var res=JSON.parse(data).response;
       console.log("the first palace is "+res.venues[0].name);
-      splash.subtitle(JSON.parse(data).response.venues[0].name);
+      splash.subtitle.text(JSON.parse(data).response.venues[0].name);
       console.log("length is "+res.venues.length);
       for (var i=0;i<res.venues.length;i++){
         console.log("place is "+res.venues[i].name); 
@@ -51,7 +73,8 @@ function locationSuccess(pos) {
 }
 
 function locationError(err) {
-  //geotext.text('location error (' + err.code + '): ' + err.message);
+  console.log('location error ('+ err.code + '): ' + err.message);
+  splash.subtitle.text('location error (' + err.code + '): ' + err.message);
 }
 navigator.geolocation.getCurrentPosition(locationSuccess, locationError, locationOptions);
 
@@ -59,9 +82,9 @@ navigator.geolocation.getCurrentPosition(locationSuccess, locationError, locatio
 
 
 function sent2emo(sentiment){
-  if (sentiment=="negative") return(" : (");
-  if (sentiment=="neutral") return(" : |");
-  if (sentiment=="positive") return(" : D");
+  if (sentiment=="negative") return(": (");
+  if (sentiment=="neutral") return(": |");
+  if (sentiment=="positive") return(": D");
   return " = D";
 }
 function sent2color(sentiment){
@@ -74,11 +97,11 @@ function sent2color(sentiment){
 function getSentimentData(places){
   ajax(
   {
-    url:'http://chetannaik.pythonanywhere.com/query/sdfsdf'//supposed to use the places as argument.
+    url:'http://chetannaik.pythonanywhere.com/query/'+JSON.stringify(places)
   },
   function(data){
     console.log('successfully fetched tweets');
-    splash.subtitle("retrieved data");
+    splash.subtitle.text("retrieved data");
     var wind = new UI.Window({fullscreen:true});
     var siz=new Vector2(36,36);
     var senti=JSON.parse(data).response;
@@ -96,7 +119,9 @@ function getSentimentData(places){
         var text=new UI.Text({
           position:pos,
           size:siz,
+          font:(senti[index].sentiment=='neutral')?"gothic-28":"gothic-28-bold",
           text:sent2emo(senti[index].sentiment),
+          textAlign:'center',
           color:(c=="black")?"white":"black",
           borderColor:"black"
         });
@@ -166,24 +191,18 @@ function getSentimentData(places){
     
   wind.add(selectorBox);
   
-  /*var geotext=new UI.Text({
-    text:"getting location data..",
-    size:new Vector2(144,16),
-    position:new Vector2(0,144),
-    color:"white"
-  });
-  wind.add(geotext);*/
   function startRandomPreview(){
       console.log('Show randam tweet preview');
       Vibe.vibrate('short');
       //send back rand
       var rand=Math.floor(Math.random()*senti.length);
       var preview=new UI.Card({
-        title:"Tweet Preview",
+        title:"Tweet",
         subtitle:sent2emo(senti[rand].sentiment),
         body:senti[rand].tweet,
         scrollable:true
       });
+      
       preview.show();
       preview.on('accelTap',function(){
         Vibe.vibrate('short');
@@ -199,11 +218,13 @@ function getSentimentData(places){
       Vibe.vibrate('short');
       //send back rand
       var preview=new UI.Card({
-        title:"Tweet Preview",
+        title:"Tweet",
         subtitle:sent2emo(senti[selector].sentiment),
         body:senti[selector].tweet,
         scrollable:true
       });
+      
+    
       preview.show();
       preview.on('accelTap',function(){
         Vibe.vibrate('short');
@@ -211,6 +232,7 @@ function getSentimentData(places){
         //send back rand
         preview.subtitle(sent2emo(senti[rand].sentiment));
         preview.body(senti[rand].tweet);
+        
       });
       
   }
@@ -219,13 +241,8 @@ function getSentimentData(places){
   },//end of main success function 
   function(error){
     console.log('Failed fetching tweets');
-    splash.subtitle("Failed to fetch tweets");
+    splash.subtitle.text("Failed to fetch tweets");
   }
 );
 
 }
-
-
-
-
-
